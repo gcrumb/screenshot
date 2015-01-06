4608d0d037e5c1fcf2fa5885bb2da99e16e95218
Dan McGarry <dmcgarry@pacificpolicy.org>
2015-01-07 10:27:54 +1100

    Initial checkin

---
+++ scripts/rasterizer.js
@@ -0,0 +1,142 @@
+/*
+ * phantomjs rasteriser server
+ *
+ * Usage:
+ *   phantomjs rasterizer.js [basePath] [port] [defaultViewportSize]
+ *
+ * This starts an HTTP server waiting for screenshot requests
+ */
+var basePath = phantom.args[0] || '/tmp/'; 
+
+var port  = phantom.args[1] || 3001;
+
+var defaultViewportSize = phantom.args[2] || '';
+defaultViewportSize = defaultViewportSize.split('x');
+defaultViewportSize = {
+  width: ~~defaultViewportSize[0] || 1024,
+  height: ~~defaultViewportSize[1] || 600
+};
+
+var pageSettings = ['javascriptEnabled', 'loadImages', 'localToRemoteUrlAccessEnabled', 'userAgent', 'userName', 'password'];
+
+var server, service;
+
+var redirectUrl = '';
+
+server = require('webserver').create();
+
+/*
+ * Screenshot service
+ *
+ * Generate a screenshot file on the server under the basePath
+ *
+ * Usage:
+ * GET /
+ * url: http://www.google.com
+ *
+ * Optional headers:
+ * filename: google.png
+ * width: 1024
+ * height: 600
+ * clipRect: { "top": 14, "left": 3, "width": 400, "height": 300 }
+ *
+ * If path is omitted, the service creates it based on the url, removing the
+ * protocol and replacing all slashes with dots, e.g
+ * http://www.google.com => www.google.com.png
+ *
+ * width and height represent the viewport size. If the content exceeds these
+ * boundaries and has a non-elastic style, the screenshot may have greater size.
+ * Use clipRect to ensure the final size of the screenshot in pixels.
+ *
+ * All settings of the WebPage object can also be set using headers, e.g.:
+ * javascriptEnabled: false
+ * userAgent: Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+
+ */ 
+service = server.listen(port, function(request, response) {
+  if (request.url == '/healthCheck') {
+    response.statusCode = 200;
+    response.write('up');
+    response.close();
+    return;
+  }
+  if (!request.headers.url) {
+    response.statusCode = 400;
+    response.write('Error: Request must contain an url header' + "\n");
+    response.close();
+    return;
+  }
+  var url = request.headers.url;
+  var path = basePath + (request.headers.filename || (url.replace(new RegExp('https?://'), '').replace(/\//g, '.') + '.png'));
+  var page = new WebPage();
+  var delay = request.headers.delay || 0;
+  try {
+    page.viewportSize = {
+      width: request.headers.width || defaultViewportSize.width,
+      height: request.headers.height || defaultViewportSize.height
+    };
+    if (request.headers.clipRect) {
+      page.clipRect = JSON.parse(request.headers.clipRect);
+    }
+    for (name in pageSettings) {
+      if (value = request.headers[pageSettings[name]]) {
+        value = (value == 'false') ? false : ((value == 'true') ? true : value);
+        page.settings[pageSettings[name]] = value;
+      }
+    }
+  } catch (err) {
+    response.statusCode = 500;
+    response.write('Error while parsing headers: ' , err);
+    return response.close();
+  }
+
+  page.onResourceReceived = function(resource) {
+    if (url == resource.url && resource.redirectURL) {
+      redirectURL = resource.redirectURL;
+    }
+  };
+
+	page.evaluate(function() {
+
+			// See: http://www.netlobo.com/javascript-insertafter.html
+			var insertAfter = function( referenceNode, newNode )
+			{
+					console.log("NODE: ", referenceNode);
+					referenceNode.parentNode.insertBefore( newNode, referenceNode.nextSibling );
+			}
+
+			var style = document.createElement('style');
+			var text = document.createTextNode('body { background: #fff }');
+
+			style.setAttribute('type', 'text/css');
+			style.appendChild(text);
+			//insertAfter(style, document.head.firstChild);
+			document.head.insertBefore(style, document.head.lastChild);
+	});
+
+	page.onError = function(msg, trace) {
+			console.log('Uh-oh',msg);
+			trace.forEach(function(item) {
+					console.log('Uh-oh', item.file, ':', item.line);
+			});
+	}
+
+  page.open(url, function(status) {
+    if (status == 'success') {
+      window.setTimeout(function () {
+        page.render(path, { format: "jpg", quality: 30 });
+				response.statusCode = 200;
+        response.write('Success: Screenshot saved to ' + path + "\n");
+        page.release();
+        response.close();
+      }, delay);
+    } else {
+			response.statusCode = 500;
+      response.write('Error: Url returned status ' + status + "\n");
+      page.release();
+      response.close();
+    }
+  });
+  // must start the response now, or phantom closes the connection
+  response.statusCode = 200;
+  response.write('');
+});
